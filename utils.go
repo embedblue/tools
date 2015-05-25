7b32cabb742291debb987c3fb6eebfaa0aa082d6
pandazhong <449678910@qq.com>
2015-05-25 20:44:02 +0800

    修改BUG

--- utils.go
+++ utils.go
@@ -105,13 +105,11 @@ func Math_round(f float64, n int)float64{
 
 /// 根据某天的某个时刻的时间戳，获取当天0:0:0的时间戳
 func GetZeroSecondsFromTimestamp(ts int64)int64{
-	/// 今天0:0:0的时间戳
 	today_0_ts := GetZeroSecondsToday()
 	past_time := today_0_ts - ts
 	if past_time > 0 {
 		return today_0_ts - int64(past_time/86400) * 86400
 	}else {
-		fmt.Printf("past_time: %d, ts: %d\n", past_time, ts)
-		return today_0_ts + 86400
+		return today_0_ts
 	}
 }
